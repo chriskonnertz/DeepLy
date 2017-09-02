a6a393bc00d977587244bb101efff92ce971c833
ChrisKonnertz <siconize@gmail.com>
2017-09-02 12:56:15 +0200

    Added ProtocolException class

---
+++ src/ChrisKonnertz/DeepLy/Protocol/ProtocolException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ChrisKonnertz\DeepLy\Protocol;
+
+/*
+ * This exception might be thrown by a class that implements the ProtocolInterface
+ */
+class ProtocolException extends \RuntimeException
+{
+
+}
