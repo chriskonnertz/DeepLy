cab59e527edc0ad36636a13bc210860edfe470ac
ChrisKonnertz <siconize@gmail.com>
2017-09-02 15:10:49 +0200

    Bugfix for accessing wrong var

--- src/ChrisKonnertz/DeepLy/TranslationBag/TranslationBag.php
+++ src/ChrisKonnertz/DeepLy/TranslationBag/TranslationBag.php
@@ -80,7 +80,7 @@ class TranslationBag
                 }
 
                 foreach ($translation->beams as $beamIndex => $beam) {
-                    if (! property_exists($translation, 'postprocessed_sentence')) {
+                    if (! property_exists($beam, 'postprocessed_sentence')) {
                         throw new TranslationBagException(
                             'DeepLy API call resulted in a malformed result - '.
                             'postprocessed_sentence property is missing in beam '.$index
