69148241f69a0befc1517e7c898e0dce56c630a3
ChrisKonnertz <siconize@gmail.com>
2017-09-02 12:59:59 +0200

    Added processResponseData() method, moved validation code to this class to the new validateResponseData() method, introduced version const

--- src/ChrisKonnertz/DeepLy/Protocol/JsonRpcProtocol.php
+++ src/ChrisKonnertz/DeepLy/Protocol/JsonRpcProtocol.php
@@ -12,6 +12,11 @@ class JsonRpcProtocol implements ProtocolInterface
 {
 
     /**
+     * The number of the supported protocol version
+     */
+    const PROTOCOL_VERSION = '2.0';
+
+    /**
      * Creates a request bag according to the JSON RPC protocol.
      * The API will be able to understand it.
      * The result is encoded as a JSON string.
@@ -30,7 +35,7 @@ class JsonRpcProtocol implements ProtocolInterface
         }
 
         $data = [
-            'jsonrpc' => '2.0', // Set the protocol version
+            'jsonrpc' => self::PROTOCOL_VERSION, // Set the protocol version
             'method' => $method, // Set the method of the JSON RPC API call
             'params' => $payload // Set the parameters / the payload
         ];
@@ -38,6 +43,74 @@ class JsonRpcProtocol implements ProtocolInterface
         $jsonData = json_encode($data);
 
         return $jsonData;
+    }
+
+    /**
+     * Processes the data from an response from the server to an API call.
+     * Returns the payload (data) of the response or throws a ProtocolException.
+     *
+     * @param string $rawResponseData The data (payload) of the response as a stringified JSON string
+     * @return \stdClass The data (payload) of the response as an object structure
+     * @throws ProtocolException
+     */
+    public function processResponseData($rawResponseData)
+    {
+        if (! is_string($rawResponseData)) {
+            throw new \InvalidArgumentException('The $rawResponseData argument has to be a string');
+        }
+
+        $responseData = json_decode($rawResponseData);
+
+        $this->validateResponseData($responseData);
+
+        // We only return the inner result object, the other properties are no longer important
+        return $responseData->result;
+    }
+
+    /**
+     * Validates the response data (usually a \stdClass built by json_decode)
+     * is valid response data from an API call to the DeepL API using the JSON RPC protocol.
+     *
+     * @param $responseData
+     * @throws ProtocolException
+     */
+    protected function validateResponseData($responseData)
+    {
+        if (! $responseData instanceof \stdClass) {
+            throw new ProtocolException('DeepLy API call did not return JSON that describes a \stdClass object');
+        }
+
+        if (! property_exists($responseData, 'jsonrpc')) {
+            throw new ProtocolException('
+                The given response data does not seem to be come from a JSON RPC request - it has no "jsonrpc" property'
+            );
+        }
+        if ($responseData->jsonrpc !== self::PROTOCOL_VERSION) {
+            throw new ProtocolException(
+                'The version of the JSON RPC response does not match the expected version '.self::PROTOCOL_VERSION
+            );
+        }
+
+        if (property_exists($responseData, 'error')) {
+            if ($responseData->error instanceof \stdClass and property_exists($responseData->error, 'message')) {
+                throw new ProtocolException(
+                    'DeepLy API call resulted in this error: '.$responseData->error->message
+                );
+            } else {
+                throw new ProtocolException('DeepLy API call resulted in an unknown error');
+            }
+        }
+
+        if (! property_exists($responseData, 'result')) {
+            throw new ProtocolException(
+                'DeepLy API call resulted in a malformed result - inner result property is missing'
+            );
+        }
+        if (! $responseData->result instanceof \stdClass) {
+            throw new ProtocolException(
+                'DeepLy API call resulted in a malformed result - inner result property is not a \stdClass'
+            );
+        }
     }
 
 }
