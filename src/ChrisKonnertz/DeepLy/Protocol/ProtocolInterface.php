45a8e13f731a9f1ed1081104049842a55a0dbf79
Chris Konnertz <siconize@gmail.com>
2017-09-03 19:56:16 +0200

    Adjusted interface so it also has the processResponseData() method

--- src/ChrisKonnertz/DeepLy/Protocol/ProtocolInterface.php
+++ src/ChrisKonnertz/DeepLy/Protocol/ProtocolInterface.php
@@ -18,5 +18,15 @@ interface ProtocolInterface
      * @return string
      */
     public function createRequestData(array $payload, $method);
-
+    
+    /**
+     * Processes the data from an response from the server to an API call.
+     * Returns the payload (data) of the response or throws a ProtocolException.
+     *
+     * @param string $rawResponseData The data (payload) of the response as a stringified JSON string
+     * @return \stdClass              The data (payload) of the response as an object structure
+     * @throws ProtocolException|\InvalidArgumentException
+     */
+    public function processResponseData($rawResponseData);
+    
 }
