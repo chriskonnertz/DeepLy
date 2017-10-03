165bc4edf76eceb8c804e58e22d8326a81095e5b
ChrisKonnertz <siconize@gmail.com>
2017-10-03 15:30:19 +0200

    --- VERSION 1.4.2 ---

--- src/ChrisKonnertz/DeepLy/DeepLy.php
+++ src/ChrisKonnertz/DeepLy/DeepLy.php
@@ -78,7 +78,7 @@ class DeepLy
     /**
      * Current version number
      */
-    const VERSION = '1.4.1';
+    const VERSION = '1.4.2';
 
     /**
      * If true, validate that the length of a translation text
