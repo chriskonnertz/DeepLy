ee708e6d8d2a1944d5d1116dace5c5be1ed3a81c
ChrisKonnertz <siconize@gmail.com>
2018-03-30 13:29:21 +0200

    Bugif for paths in demo autoloader

--- demos/demo_ping.php
+++ demos/demo_ping.php
@@ -7,7 +7,7 @@
  */
 function miniAutoloader($class)
 {
-    $class = str_replace('\', '/', $class);
+    $class = str_replace('\\', '/', $class);
     require __DIR__ . '/../src/' . $class . '.php';
 }
 
