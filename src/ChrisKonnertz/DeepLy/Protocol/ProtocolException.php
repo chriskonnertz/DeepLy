fa3a5abc1ef3a337a12267eb424e1324068a0eb1
Chris Konnertz <siconize@gmail.com>
2019-03-11 17:06:01 +0100

    Added missing asterisk

--- src/ChrisKonnertz/DeepLy/Protocol/ProtocolException.php
+++ src/ChrisKonnertz/DeepLy/Protocol/ProtocolException.php
@@ -2,7 +2,7 @@
 
 namespace ChrisKonnertz\DeepLy\Protocol;
 
-/*
+/**
  * This exception might be thrown by a class that implements the ProtocolInterface
  */
 class ProtocolException extends \RuntimeException
